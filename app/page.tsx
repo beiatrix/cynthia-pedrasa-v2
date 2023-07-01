dd511e1a613b9b90c1d16373fbecc2eeaeb4b413
beiatrix <bpedrasa@gmail.com>
2023-07-01 16:01:46 -0700

    feat: setup TypeScript types

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import { getProjects } from '@/sanity/sanity-utils'
 
 export default async function Home() {
-  const projects = await getProjects();
+  const projects = await getProjects()
 
   return (
     <div>
-      {projects.map((project) => (
+      {
+        projects.map((project) => (
           <div key={project._id}>
             {project.name}
           </div>
-        ))}
+        ))
+      }
     </div>
   )
 }
