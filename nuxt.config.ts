16b1d5be17acc4809db3d515d959ab1227913b73
beiatrix <bpedrasa@gmail.com>
2024-07-20 17:50:40 -0500

    feat: add google fonts, page meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,37 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Cynthia Pedrasa',
+      meta: [
+        {
+          "name": "viewport",
+          "content": "width=device-width, initial-scale=1"
+        },
+        {
+          "charset": "utf-8"
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'I am a Registered Nurse working as a Clinical Informatics Specialist at Northwell Health. Computers, information science, and their applications in the healthcare setting have always fascinated me.'
+        }
+      ],
+    },
+  },
   compatibilityDate: '2024-04-03',
   devtools: {
     enabled: true
   },
+  googleFonts: {
+    display: 'swap',
+    families: {
+      'DM Sans': true
+    }
+  },
   modules: [
+    '@nuxtjs/google-fonts',
     '@nuxtjs/tailwindcss'
   ]
 })
